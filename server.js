0af15955fdbbe0e0449feb49c60fd31260934104
Yash Parwani <parwaniyash5610@gmail.com>
2023-08-31 23:52:55 +0530

    making changes again

--- server.js
+++ server.js
@@ -9,19 +9,20 @@ app.use(express.json());
 
 const baseRouter = express.Router();
 
+
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('hello world!!');
+    return res.send('Hello World!');
 });
 
 baseRouter.post('/add', (req, res) => {
-    const {num1,num2} = req.body;
-    res.json({ "result": num1+num2 });
+    const { first, second } = req.body;
+    res.json({ "result": first + second });
 });
 
 
 baseRouter.post('/subtract', (req, res) => {
-    const {num1,num2} = req.body;
-    res.json({ "result": num1-num2 });
+    const { first, second } = req.body;
+    res.json({ "result": first - second });
 });
 
 app.use(baseUrl, baseRouter);
